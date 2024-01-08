ddc6f790e96a41b4cf8244601ed1b7e2fed2ce75
kamit1905 <amit1.naukri@gmail.com>
2024-01-09 01:13:59 +0530

    New

--- src/tests/ATestAutomationDemo1.java
+++ src/tests/ATestAutomationDemo1.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
-public class ATestAutomationDemo1 {
+public class ATestAutomationDemo1 {		//changes done
 	
 	public static void main(String[] args) throws InterruptedException {
 		
